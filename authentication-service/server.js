3b2537b18102e11a35b6785eb3c24e275d91ed30
Giclops <atchleyne@gmail.com>
2022-01-28 12:56:59 -0700

    added basic login

--- authentication-service/server.js
+++ authentication-service/server.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const port = 3000
 
 const app = express();
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     res.send('Hello Browser');
 });
+
+app.post('/login', (req, res) => {
+    console.log(JSON.stringify(req.body));
+    if(req.body.userName =="Giclops" && req.body.password=="P@ssw0rd"){
+        res.send("Welcome!");
+    } else {
+        res.send("Get thee hence, HACKER!!");
+    }
+})
+
 app.listen(port, ()=>{});
