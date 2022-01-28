fd19cce48487e6ce117fd21492e618b2b2f7fb59
Giclops <atchleyne@gmail.com>
2022-01-28 13:53:25 -0700

    Hashed the pass/username, privated data.json

--- authentication-service/server.js
+++ authentication-service/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const port = 3000
+const md5 = require('md5');
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.get('/', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    console.log(JSON.stringify(req.body));
-    if(req.body.userName =="Giclops" && req.body.password=="P@ssw0rd"){
+    let passHash = md5(req.body.password);
+    let userHash = md5(req.body.userName);
+    if(userHash =='eb3d6ae2d348f2295f649048cd58ca23' && passHash=='189972a5f5418599d4e38031896721a4'){
         res.send("Welcome!");
     } else {
         res.send("Get thee hence, HACKER!!");
